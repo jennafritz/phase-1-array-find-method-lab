759e8def399dcbae7a58e8dc3eb4e3297d206bec
jennafritz <jennafritz24@gmail.com>
2021-04-07 12:21:17 -0400

    commit tree ccd8235bc8634148e8b24d30f11023b6f8698be8

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+// function superbowlWin(array) {
+//     array.find(function(element) {
+//         return element.result === "W"
+//     })
+// }
 
+
+    function superbowlWin(array) {
+        let win = array.find(function(element) {
+            return element.result === "W"
+        })
+        if (!!win === true) {
+            return win.year
+        }
+    }
+
+// function superbowlWin(array) {
+//     array.find(function (element) {
+//         if (element.result === "W") {
+//             return win = element
+//         }
+//     })
+// }
